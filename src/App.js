45ac15e322c94d0889a839826a70bb595c48b363
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-07 12:09:51 -0500

    Add styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import './App.css';
 
+var defaultString = "Not Provided.";
+
 // ContactCard component
 class ContactCard extends Component {
   render() {
-    return <div id={this.props.contactName}>
+    return <div id={this.props.contactName} className="contactCard">
       <h3>Name: {this.props.contactName}</h3>
       <h3>Mobile: {this.props.mobileNumber}</h3>
-      <h3>Work: {this.props.workNumber}</h3>
+      <h3>Work: {this.props.workNumber ? this.props.workNumber: defaultString}</h3>
       <h3>Email: {this.props.email}</h3>
     </div>;
   }
@@ -24,7 +26,14 @@ ContactCard.propTypes = {
 
 class App extends Component {
   render() {
-    return <ContactCard contactName="John Doe" mobileNumber="8675309" email="john@doe.com"/>
+    return <div>
+      <h1 id="title">Your Contacts: </h1>
+      <div id="contactCardGallery">
+        <ContactCard contactName="John Doe" mobileNumber="8675309" email="john@doe.com"/>
+        <ContactCard contactName="Jane Doe" mobileNumber="1234567" workNumber="9876543" email="jane@doe.com"/>
+        <ContactCard contactName="Jack Doe" mobileNumber="7654321" email="jack@doe.com"/>
+      </div>
+    </div>;
   }
 }
 
